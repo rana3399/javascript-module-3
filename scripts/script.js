6e986eec705d3d907af16f3e177c3eb177111da2
Rana Ahmed <foster3399@gmail.com>
2021-08-28 23:21:01 +0200

    More Css

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,71 @@
-//  episodes = async function (){
-//  let episodesURL = "https://rickandmortyapi.com/api/episode"
+
+// --------------------------------------------Episodes
+  episodes = async function (){
+    let episodesURL = "https://rickandmortyapi.com/api/episode"
  
-//  let resposne = await fetch(episodesURL);
-//  let {results} = await resposne.json();
-//  console.log(results)
-//    // return results;
-//    results.forEach(({name, air_date, episode}) =>{
-//        console.log(
-//            `The name is ${name}`,
-//            `The air date is ${air_date}`,
-//            `and the episode code is: ${episode}`
-//         )
-//    })
-// }
+ let resposne = await fetch(episodesURL);
+ let {results} = await resposne.json();
+ console.log(results)
+   // return results;
+   results.forEach(({name, air_date, episode}) =>{
+       console.log(
+           `The name is ${name}`,
+           `The air date is ${air_date}`,
+           `and the episode code is: ${episode}`
+        )
+   })
 
-// episodes();
+  }
+  episodes()
 
-// ----------------------
+  // ------------------------------------------------charecter
+
+
+
 charecter = async function (){
     const charecterURL = "https://rickandmortyapi.com/api/character";
     
     let resposne = await fetch(charecterURL);
      let {results} = await resposne.json();
-    //  results.map((results)=>{
-    //     console.log(results.episode)
-    //  })
-    
+     results.map((results)=>{
+        console.log(results.episode);
+     })
+     let rootEl = document.getElementById('root');
     // return results;
-    //    results.forEach(({name, status, species, gender}) =>{
-    //     let rootEl = document.getElementById('root');
-    //     let div = document.createElement('div');
-    //     //let imgEl = document.createElement('img');
+     results.map((result)=>{
+        const box = document.createElement('div');
+        let imgEl = document.createElement('img');
+            imgEl.classList.add('main-img')
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        box.classList.add('box');
+console.log(result.image);
+
+         imgEl.src = result.image;
+        p1.innerHTML = `SPECIES: ${result.species}`;
+        p2.innerHTML = `STATUS: ${result.status}`;
         
-    //     imgEl.src = `${image}`;
-    //     div.appendChild(imgEl)
-    //     rootEl.appendChild(div);
-    //       console.log(
-    //           `The name is ${name}`,
-    //           `The status of the Charecter is :  ${status}`,
-    //           `and the species is: ${species}`,
-    //           `Gender is: ${gender}`
-    //        )
-    //   })
+
+        box.appendChild(imgEl)
+        box.appendChild(p1)
+        box.appendChild(p2)
+       const main = rootEl.childNodes[4]
+       
+       main.appendChild(box);
+      // main.appendChild(box);
+       console.log(main)
+       
+     })
       return results;
    }
 
-  
+   charecter();
    
-
-// ---------------
+ // -------------------------------head
 
 let rootEl = document.getElementById('root');
 
-let creatHeader = function(){
+let headFunc = function(){
 let headContainer = document.createElement('div');
     headContainer.classList.add('headContainerClass');
 
@@ -67,13 +80,36 @@ let hTag = document.createElement('h1')
     
 }
 
- creatHeader()
+headFunc()
+
+ // //----------------------------mainContainer
+ 
+let mainContainer = function(){
+  //  let results = await charecter();
+  let main = document.createElement('div');
+  main.classList.add('main');
+    let insideContainer = document.createElement('div');
+    insideContainer.classList.add('inside-main');
+         //let flexContainer = document.createElement('div');
+        //flexContainer.classList.add('flex-box');
+        
+     //   const sideEl = document.getElementsByClassName("headContainerClass");
+        main.appendChild(insideContainer);
+        rootEl.appendChild(main);
+       // console.log(sideEl)
+      //  console.log(results);
+
+}
+
+mainContainer();
+
+// // -------------------------------------------------------sideBarContainer
 
 let sideBarContainer = async function(){
 
     let results = await charecter();
   
-    //console.log(results);
+    console.log(results);
     sideBarEl = document.createElement('div');
      sideBarEl.classList.add('sideBar-container');
 
@@ -85,26 +121,28 @@ let sideBarContainer = async function(){
 
     let ul = document.createElement("ul");
     results.map((result,index)=>{
-        // console.log(results.episode)
+         console.log(results.episode)
         let li =document.createElement("li")
         let links = document.createElement("a");
 
-        //links.setAttribute('title', 'Google');
-        //links.setAttribute('href', 'http://google.com');
-        //links.title= `Episode ${index+1}`;
-        //links.href = result.episode;
         links.text  = `Episode ${index+1}` 
         links.href  = result.episode[index]
         links.target = "_blank";
 
-         
         li.appendChild(links);
         ul.appendChild(li);
         sideBarNavLinks.appendChild(ul);
 
-     })
+    });
+    let buttonEl = document.createElement('button');
+    buttonEl.classList.add('sideBar-btn');
+    buttonEl.textContent = 'Load';
 
+    sideBarNavLinks.appendChild(buttonEl)
 
 }
 
-sideBarContainer()
+sideBarContainer();
+
+
+
